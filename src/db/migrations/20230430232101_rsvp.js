bff6af98edb5b30c73380c4f1892b4ef803b050c
ginniss2022 <gabevaughninn@gmail.com>
2023-04-30 21:46:49 -0400

    finished crud for rsvp feature

---
+++ src/db/migrations/20230430232101_rsvp.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => knex.schema.createTable('rsvp', (table) => {
+  table.integer("post_id").notNullable();
+  table.integer('user_id').notNullable();
+});
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable('rsvp');
