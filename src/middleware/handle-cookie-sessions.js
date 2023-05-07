77ea351e94522447528c1bfe11a3a8a5c8b6e8a5
cliffentei <cliffeti776@gmail.com>
2023-05-07 17:38:56 -0400

    Push

--- src/middleware/handle-cookie-sessions.js
+++ src/middleware/handle-cookie-sessions.js
@@ -3,8 +3,10 @@ const cookieSession = require('cookie-session');
 
 const Router = express.Router();
 
+const SESSION_SECRET = 'db8c3cffebb2159b46ee38ded600f437ee080f8605510ee360758f6976866e00d603d9b3399341b0cd37dfb8e599fff3';
+
 Router.use(cookieSession({
-  secret: process.env.SESSION_SECRET,
+  secret: SESSION_SECRET,
   // By default, the cookie's lifetime is "session"
   // which means until we close the browser. We like this for now!
   // But in real life you'd set the cookie to expire,
